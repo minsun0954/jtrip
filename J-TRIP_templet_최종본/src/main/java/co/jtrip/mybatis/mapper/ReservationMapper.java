1f1aa755675460ba8586da8f6f6f2a3e2caf68e7
정민선 <@192.168.219.105>
2023-01-10 19:58:40 +0900

    d

--- J-TRIP_templet_최종본/src/main/java/co/jtrip/mybatis/mapper/ReservationMapper.java
+++
@@ -1,9 +0,0 @@
-package co.jtrip.mybatis.mapper;
-
-import co.jtrip.reservation.service.ReservationVO;
-
-public interface ReservationMapper {
-	
-	ReservationVO reservationSelect(ReservationVO vo);
-	int reservationInsert(ReservationVO vo);
-}
--- J-TRIP_templet_최종본/src/main/java/co/jtrip/reservation/service/ReservationService.java
+++
@@ -1,9 +0,0 @@
-package co.jtrip.reservation.service;
-
-public interface ReservationService {
-	
-	ReservationVO reservationSelect(ReservationVO vo);
-	int reservationInsert(ReservationVO vo);
-	
-	
-}
